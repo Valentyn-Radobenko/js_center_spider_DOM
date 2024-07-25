e10f3d4a0bf0ee7c1b2d4e8d82dde321e6292735
Valentyn Radobenko <valikradobenko@gmail.com>
2024-07-25 17:48:33 +0300

    make build

---
+++ index.e684c7f5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall");e.style.display="flex",e.style.alignItems="center",e.style.justifyContent="center",document.querySelector(".spider").style.position="static";
+//# sourceMappingURL=index.e684c7f5.js.map
